191f27a61255707335b6e56e0e62fe29ccd575b7
Abi Franklin <abifranklin@gmail.com>
2024-06-13 21:06:20 -0500

    Walls drawn

--- game.js
+++ game.js
@@ -8,6 +8,13 @@ let createRect = (x, y, width, height, color) => {
     canvasContext.fillRect(x, y, width, height)
 }
 
+let fsp = 30;
+let oneBlockSize = 20
+let wallColor = "#342DCA"
+let wallInnerColor = "black"
+let wallSpaceSize = oneBlockSize / 1.5
+let wallOffset = (oneBlockSize - wallSpaceSize) / 2
+
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -45,4 +52,30 @@ let update = () => {
 
 let draw = () => {
     //Write Code
+    createRect(0, 0, canvas.width, canvas.height, "black")
+    drawWalls()
+}
+
+let gameInterval = setInterval(gameLoop, 1000 / fsp);
+
+let drawWalls = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 1) {
+                createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor)
+                if (j > 0 && map[i][j - 1] == 1) {
+                    createRect(j * oneBlockSize, i * oneBlockSize + wallOffset, wallSpaceSize + wallOffset, wallSpaceSize, wallInnerColor)
+                }
+                if (j < map[0].length - 1 && map[i][j + 1] === 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize + wallOffset, wallSpaceSize + wallOffset, wallSpaceSize, wallInnerColor)
+                }
+                if (i > 0 && map[i-1][j] == 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize, wallSpaceSize, wallSpaceSize + wallOffset, wallInnerColor)
+                }
+                if (i < map.length - 1 && map[i + 1][j] === 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize + wallOffset, wallSpaceSize, wallSpaceSize + wallOffset, wallInnerColor)
+                }
+            }
+        }
+    }
 }
